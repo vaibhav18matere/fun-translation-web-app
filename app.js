47f5140ec2c6258ee964db45054f0623cfe11f2a
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-30 15:09:33 +0530

    added serverURL and logic

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-console.log("Working!");
-
 const btnTranslator = document.querySelector("#btn-translator");
 const txtInput = document.querySelector("#txt-input");
 const outputDiv = document.querySelector("#output");
 
-clickHandler = () => {
-    console.log(txtInput.value);
+var serverURL = "https://api.funtranslations.com/translate/russian-accent.json";
+
+getTranslationURL = (input) => {
+    return serverURL + "?" + "text=" + input
 }
 
+errorHandler = (error) => {
+    console.log("error ocuured", error);
+    alert("something went wrong");
+}
 
+clickHandler = () => {
 
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+
+        .then(response => response.json())
+        .then(json => {
+
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        //error handling
+        .catch(errorHandler)
+}
 
 btnTranslator.addEventListener("click", clickHandler)
 
