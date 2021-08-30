ae040a773c59881ea338919dc57746e8a2763ca9
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-30 13:11:40 +0530

    import everthing in js file and checked wiring

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Working!");
+
+const btnTranslator = document.querySelector("#btn-translator");
+const txtInput = document.querySelector("#txt-input");
+const outputDiv = document.querySelector("#output");
+
+clickHandler = () => {
+    console.log(txtInput.value);
+}
+
+
+
+
+btnTranslator.addEventListener("click", clickHandler)
+
